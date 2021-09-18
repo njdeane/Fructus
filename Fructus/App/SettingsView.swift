6e85a47aff2ea43d64c6d4330ea8a5898a407d51
njdeane <njdeane7@gmail.com>
2021-09-18 13:37:47 +1000

    Create group box in settingsView.

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -8,34 +8,65 @@
 import SwiftUI
 
 struct SettingsView: View {
-    
-    @Environment(\.presentationMode) var presentationMode
-    
-    
-    var body: some View {
-        NavigationView {
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack(spacing: 20) {
-                    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                }
-                .navigationBarTitle(Text("Settings"), displayMode: .large)
-                .navigationBarItems(
-                    trailing:
-                        Button(action: {
-                            presentationMode.wrappedValue.dismiss()
-                        }) {
-                            Image(systemName: "xmark")
-                        }
-                )
-                .padding()
+  
+  @Environment(\.presentationMode) var presentationMode
+  
+  
+  var body: some View {
+    NavigationView {
+      ScrollView(.vertical, showsIndicators: false) {
+        VStack(spacing: 20) {
+          // Section 1
+          GroupBox(
+            label:
+              SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+          ) {
+            Divider().padding(.vertical, 4)
+            
+            HStack(alignment: .center, spacing: 10) {
+              Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .cornerRadius(9)
+              
+              Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietry fiber, vitamins, and much more.")
+                .font(.footnote)
             }
+          }
+          // Section 2
+          
+          // Section 3
+          GroupBox(
+            label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+          ) {
+            Divider().padding(.vertical, 4)
+            
+            HStack {
+              Text("Developer").foregroundColor(Color.gray)
+              Spacer()
+              Text("Pirate Apps Inc")
+            }
+          }
         }
+        .navigationBarTitle(Text("Settings"), displayMode: .large)
+        .navigationBarItems(
+          trailing:
+            Button(action: {
+              presentationMode.wrappedValue.dismiss()
+            }) {
+              Image(systemName: "xmark")
+            }
+        )
+        .padding()
+      }
     }
+  }
 }
 
 struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView()
-            .preferredColorScheme(.dark)
-    }
+  static var previews: some View {
+    SettingsView()
+      .preferredColorScheme(.dark)
+  }
 }
