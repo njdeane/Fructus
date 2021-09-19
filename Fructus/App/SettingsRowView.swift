ce88ed3b3320389cea15b34b2f9db1e7b9c36f87
Nic Deane <njdeane7@gmail.com>
2021-09-19 15:28:51 +1000

    New computer.

---
+++ Fructus/App/SettingsRowView.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsRowView.swift
+//  Fructus
+//
+//  Created by Nic Deane on 18/9/21.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+  
+    var body: some View {
+      HStack {
+        Text("Developer").foregroundColor(Color.gray)
+        Spacer()
+        Text("Pirate Apps Inc")
+      }
+    }
+  
+}
+
+struct SettingsRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsRowView()
+          .previewLayout(.fixed(width: 375, height: 60))
+          .padding()
+    }
+}
