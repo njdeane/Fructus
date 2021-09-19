b5ae8b67ed26cfd4cad517239e163866364cedab
Nic Deane <njdeane7@gmail.com>
2021-09-19 16:30:32 +1000

    Add toggle to settings view. Each time user toggles it will reset the app so they can see the onboarding again.

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SettingsView: View {
   
   @Environment(\.presentationMode) var presentationMode
+  @AppStorage("isOnboarding") var isOnboarding: Bool = false
   
   
   var body: some View {
@@ -35,7 +36,21 @@ struct SettingsView: View {
             }
           }
           // Section 2
-          
+          GroupBox(
+          label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+          ) {
+            Divider().padding(.vertical, 4)
+            Text("If you wish you can restart the application by toggling the switch in this box. That will start the onboarding process and you will see the welcome screen again.")
+              .padding(.vertical, 8) // adds padding to the top of the text
+              .frame(minHeight: 60) // minumum height of the text view
+              .layoutPriority(1) // this ensures all the text will be displayed as views default is 0 so some text can be truncated
+              .font(.footnote)
+              .multilineTextAlignment(.leading)
+            
+            Toggle(isOn: $isOnboarding) { // The $ makes it a 'binding property' so each time user toggles it updates the app storage (prior user defaults). each toggle will either change property of isOnboarding to true or false.. fucking amazing!
+              Text("Restart".uppercased())
+            }
+          }
           
           // Section 3
           GroupBox(
